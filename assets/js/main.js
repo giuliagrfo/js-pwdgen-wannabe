522b81503eabb18c75c9556bdadd8770b28e713e
giuliagrfo <giuliagrifo1@gmail.com>
2022-10-05 14:49:11 +0200

    name request

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
+// Chiedi all’utente il suo nome
+const firstName = prompt('Come ti chiami?')
+
+
+// Chiedi il suo cognome
+
+
+// Chiedi il suo colore preferito
+
+
+// Infine scrivi sulla pagina nomecognomecolorepreferito22
+
+
 
