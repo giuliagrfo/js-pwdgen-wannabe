04e49420366c1093ce80a4d9cac9d5950b32f768
giuliagrfo <giuliagrifo1@gmail.com>
2022-10-05 16:15:54 +0200

    fix code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,17 @@
 // Chiedi all’utente il suo nome
-const firstName = prompt('Come ti chiami?')
+const firstName = prompt('Come ti chiami?');
 
 
 // Chiedi il suo cognome
-const surname = prompt('Inserisci il tuo cognome')
+const surname = prompt('Inserisci il tuo cognome');
 
 
 // Chiedi il suo colore preferito
-const favorite_color = prompt('Qual è il tuo colore preferito?')
+const favoriteColor = prompt('Qual è il tuo colore preferito?');
 
 
 // Infine scrivi sulla pagina nomecognomecolorepreferito22
-const password = firstName + surname +favorite_color + '22'
+const password = firstName + surname +favoriteColor + '22';
 
-document.getElementById('password').innerHTML = `Ciao ${firstName}, la tua password è: ${password}`
+document.getElementById('password').innerHTML = `Ciao ${firstName}, la tua password è: ${password}`;
 
